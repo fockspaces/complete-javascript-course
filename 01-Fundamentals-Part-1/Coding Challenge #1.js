1e6d4521edc680f991f0d3652c05afc7942186e3
fockspace <a86gj3sp4@gmail.com>
2022-05-08 18:41:40 +0800

    chllenge 1

---
+++ 01-Fundamentals-Part-1/Coding Challenge #1.js
@@ -0,0 +1,9 @@
+    let Mark_m = 78;
+let Mark_h = 1.69;
+let John_m = 92;
+let John_h = 1.95;
+
+let Mark_BMI = Mark_m / Mark_h**2;
+let John_BMI = John_m / John_h**2;
+
+let markHigherBMI = Mark_BMI > John_BMI;
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,4 @@
+let country = 'Taiwan';
+let continent = 'Asia';
+let population = 23;
+console.log(country, continent, population);
