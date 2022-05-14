41a13627a3c6c818a58a112764ecb1a8d2ddb0aa
fockspace <a86gj3sp4@gmail.com>
2022-05-14 16:53:19 +0800

    modal complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,11 +5,26 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal
-  });
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function key(event) {
+  if(event.key == 'Escape' && !modal.classList.contains('hidden')){
+    closeModal();
+  }
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', key);
