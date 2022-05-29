517ee666c0cdfb75fd6fc815c64dd0e340356968
fockspace <a86gj3sp4@gmail.com>
2022-05-29 14:13:05 +0800

    add tab component - 1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,13 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -48,3 +49,44 @@ document
     message.parentElement.removeChild(message);
   });
 
+// smooth scroll
+btnScrollTo.addEventListener('click', function (e) {
+  // smooth scrolling
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+const h1 = document.querySelector('h1');
+h1.addEventListener('click', alertH1);
+
+// Page navigation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     const section = document.querySelector(id);
+//     section.scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. add event to parent element
+// 2. determine what element originated
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    const section = document.querySelector(id);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operation__content');
+
+tabs.forEach
