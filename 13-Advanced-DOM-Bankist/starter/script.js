ed57c6f71a7b52ce885e37ed4cbda0642387b466
fockspace <a86gj3sp4@gmail.com>
2022-05-29 16:14:37 +0800

    finish tab switch function

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -87,6 +87,20 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 // Tabbed component
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operation__content');
+const tabsContent = document.querySelectorAll('.operations__content');
 
-tabs.forEach
+// Tab switch -- Event delegation
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  // Guard clause
+  if (!clicked) return;
+  // Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+  // remove content area
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  // Active content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
